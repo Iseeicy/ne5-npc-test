dcc91220612728faf9941b5b2650b54ae123fcba
Iseeicy <iseeicyhawkston@gmail.com>
2024-05-21 18:10:41 -0500

    Started writing pathing

--- Tester.gd
+++ Tester.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var sim_input: SimulatedInput
+@export var nav: NavigationAgent3D
+@export var target: Node3D
 
 var move_axis := InputAxis2d.new(
 	InputAxis1d.new("player_move_left", "player_move_right"),
@@ -13,13 +15,26 @@ var look_axis: InputAxis2d = InputAxis2d.new(
 )
 
 
+func _physics_process(_delta):
+	nav.set_target_position(target.global_position)
+
+
 func _on_timer_timeout():
 	print("Trying jump")
-	sim_input.simulate_action_oneshot("player_jump")
+	nav.set_target_position(target.global_position)
+	# sim_input.simulate_action_oneshot("player_jump")
 
 
 func _process(_delta):
-	sim_input.simulate_axis_2d(move_axis, Vector2.UP)
+	if not nav.is_navigation_finished():
+		var movement_dir: Vector3 = get_parent().global_position.direction_to(
+			nav.get_next_path_position()
+		)
+		sim_input.simulate_axis_2d(
+			move_axis, Vector2(movement_dir.x, movement_dir.z)
+		)
 
-	var look_dir = Vector2.LEFT * _delta * 1000
-	sim_input.simulate_axis_2d(look_axis, look_dir)
+	# sim_input.simulate_axis_2d(move_axis, Vector2.UP)
+
+	# var look_dir = Vector2.LEFT * _delta * 1000
+	# sim_input.simulate_axis_2d(look_axis, look_dir)
