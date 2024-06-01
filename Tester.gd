6b714aff99598dde5412a137c1d8832b8630a2e5
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-06-01 01:24:10 -0500

    Working on follow script

--- Tester.gd
+++ Tester.gd
@@ -4,25 +4,14 @@ extends Node
 @export var nav: NavigationAgent3D
 @export var target: Node3D
 
-var move_axis := InputAxis2d.new(
-	InputAxis1d.new("player_move_left", "player_move_right"),
-	InputAxis1d.new("player_move_forward", "player_move_back")
-)
-
-var look_axis: InputAxis2d = InputAxis2d.new(
-	InputAxis1d.new("player_look_left", "player_look_right"),
-	InputAxis1d.new("player_look_down", "player_look_up")
-)
-
 
 func _physics_process(_delta):
 	nav.set_target_position(target.global_position)
 
 
 func _on_timer_timeout():
-	print("Trying jump")
+	print("Updating pos")
 	nav.set_target_position(target.global_position)
-	# sim_input.simulate_action_oneshot("player_jump")
 
 
 func _process(_delta):
@@ -31,10 +20,6 @@ func _process(_delta):
 			nav.get_next_path_position()
 		)
 		sim_input.simulate_axis_2d(
-			move_axis, Vector2(movement_dir.x, movement_dir.z)
+			BobboInputs.Player.Move.axis,
+			Vector2(movement_dir.x, movement_dir.z)
 		)
-
-	# sim_input.simulate_axis_2d(move_axis, Vector2.UP)
-
-	# var look_dir = Vector2.LEFT * _delta * 1000
-	# sim_input.simulate_axis_2d(look_axis, look_dir)
